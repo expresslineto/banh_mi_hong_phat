5327c6732dcf1ebd4ff56be4aec6af2f56306a6e
Henry Hieu Nguyen <hieunugent@gmail.com>
2022-03-07 18:35:16 -0800

    Updates

---
+++ precache-manifest.48fd991c47a3c3053b444e431fd10086.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e241457643a35a8d56f9aac5a38991ab",
+    "url": "/banh_mi_hong_phat/index.html"
+  },
+  {
+    "revision": "299b278822a6c47c7256",
+    "url": "/banh_mi_hong_phat/static/css/main.d2f390e0.chunk.css"
+  },
+  {
+    "revision": "960323c2513432f5f0b6",
+    "url": "/banh_mi_hong_phat/static/js/2.a93ee04d.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/banh_mi_hong_phat/static/js/2.a93ee04d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "299b278822a6c47c7256",
+    "url": "/banh_mi_hong_phat/static/js/main.28399aa8.chunk.js"
+  },
+  {
+    "revision": "ee0da5a872062a0bcbab",
+    "url": "/banh_mi_hong_phat/static/js/runtime-main.3e1f956f.js"
+  },
+  {
+    "revision": "f64db185592762b9556a892d2ab17b9a",
+    "url": "/banh_mi_hong_phat/static/media/banh mi hong phat menu-01.f64db185.jpg"
+  },
+  {
+    "revision": "14cbde6de7101d75e96d139e9ed09ef4",
+    "url": "/banh_mi_hong_phat/static/media/banh mi hong phat menu-02.14cbde6d.jpg"
+  },
+  {
+    "revision": "e7be4ee14d2f311932193ead354c5cb9",
+    "url": "/banh_mi_hong_phat/static/media/banh mi hong phat menu-03.e7be4ee1.jpg"
+  },
+  {
+    "revision": "dc74e6304f5c50e2948949bacbad7871",
+    "url": "/banh_mi_hong_phat/static/media/banh mi hong phat menu_tv 55- 1 copy 2.dc74e630.jpg"
+  },
+  {
+    "revision": "eef6dc5050b9b3db96c82a21ca7bc2b4",
+    "url": "/banh_mi_hong_phat/static/media/banh mi hong phat menu_tv 55- 1 copy 3.eef6dc50.jpg"
+  },
+  {
+    "revision": "78765849294c35d663eb4ffdd9d71fc4",
+    "url": "/banh_mi_hong_phat/static/media/banh mi hong phat menu_tv 55- 1 copy 4.78765849.jpg"
+  },
+  {
+    "revision": "0f5d728d89a192b7cdca54b37e30d33e",
+    "url": "/banh_mi_hong_phat/static/media/banh mi hong phat menu_tv 55- 1 copy.0f5d728d.jpg"
+  }
+]);
