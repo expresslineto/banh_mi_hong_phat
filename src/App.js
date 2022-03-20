54cc27592137f3b21c351f1a95eefb0b9c9e17b2
hieunugent <hieunugent@gmail.com>
2022-03-19 22:39:05 -0700

    update

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 import MenuForm from "./menu";
 import list from "./image/image";
 
-
+import logo from "./image/logo.png";
 import { makeStyles } from "@material-ui/core/styles";
 import { Paper, Grid } from "@material-ui/core";
 
@@ -12,7 +12,6 @@ const useStyles = makeStyles((theme) => ({
   img: {
     margin: 0,
     display: "flex",
-
     maxWidth: "100px",
     backgroundColor: "white",
     alignItems: "center",
@@ -20,6 +19,8 @@ const useStyles = makeStyles((theme) => ({
   storeInfo: {
     fontFamily: "Helvetica",
     fontStyle: "italic",
+    color: "#13274b",
+    
   },
 }));
 
@@ -32,11 +33,16 @@ function App() {
         <Grid container>
           <Grid xs={4}>
             <figure className={classes.img}>
-              
+              <img className={classes.img} src={logo} alt="store" />
             </figure>{" "}
           </Grid>{" "}
-          <Grid xs={8} className={classes.storeInfo}>
-            <h4> </h4> <h4> </h4>{" "}
+          <Grid xs={4} className={classes.storeInfo}>
+            <h5>phone: 408-223-6828 | 408-508-8179 </h5>
+            <h5>address: 1928 Aborn Road , San Jose, CA 95121</h5>{" "}
+          </Grid>
+          <Grid xs={4} className={classes.storeInfo}>
+            <h4> Open 7 Days A Week</h4>
+            <h4> 6:00am - 8:00pm</h4>
           </Grid>
         </Grid>
       </Paper>
